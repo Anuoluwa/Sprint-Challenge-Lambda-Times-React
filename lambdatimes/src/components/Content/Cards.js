2ad8cfe41e85f3b17dc38a81c775b83faaca07a4
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-07 09:36:16 +0100

    create content all the cards and add logic to the existing code

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import propTypes from 'prop-types';
+import uuid from 'uuid';
+
+
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map(card => {
+        return(
+          <Card key={uuid()} card={card} />
+        );
+      })}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  card: propTypes.arrayOf(propTypes.object)
+}
 
 export default Cards;
